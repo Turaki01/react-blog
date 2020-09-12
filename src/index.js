5252c5d4a9380e77fd0e48080de2af491233b2ff
Turaki01 <ezekiel_zakari@yahoo.com>
2020-09-12 20:01:09 +0100

    component creation

--- src/index.js
+++ src/index.js
@@ -4,12 +4,18 @@ import { BrowserRouter, Route } from 'react-router-dom'
 import * as serviceWorker from './serviceWorker';
 import Navbar from './components/Navbar';
 import Welcome from './components/Welcome';
+import Footer from './components/Footer';
+import CreateArticle from './components/CreateArticle';
 
 ReactDOM.render(
   <BrowserRouter>
     <div>
       <Navbar />
       <Route exact path="/" component={Welcome} />
+
+      <Route path="/articles/create" component={CreateArticle} />
+
+      <Footer />
     </div>
   </BrowserRouter>
   , document.getElementById('root')
